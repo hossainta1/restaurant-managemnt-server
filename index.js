1b834f0ed52cbe11873c0244537d8bf66a0cd586
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-03-12 20:40:52 +0600

    Create delete cart api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require("cors");
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -60,6 +60,15 @@ async function run() {
       const result = await cartCollection.insertOne(cartItems);
       res.send(result);
     });
+
+    // delete cart items
+     app.delete('/carts/:id', async(req, res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+     })
+    // delete cart items
 
     // Start the Server **AFTER** MongoDB is connected
     app.listen(port, () => {
