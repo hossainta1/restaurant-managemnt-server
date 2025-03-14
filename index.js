0b7d0876b06e5ea22f8ac416972df67683d3d977
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-03-14 21:01:18 +0600

    Implement JWT verification for User

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require("cors");
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
@@ -31,11 +32,55 @@ async function run() {
     const reviewCollection = client.db("restaurentdb").collection("reviews");
     const cartCollection = client.db("restaurentdb").collection("carts");
 
+    // Jwt related API start
+     app.post('/jwt', async(req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1hr'});
+      res.send({token});
+
+     })
+    // Jwt related API end
+
+    // middleware verify token start
+      const verifyToken = (req, res, next) => {
+        console.log('inside verify token', req.headers.authorization);
+        if(! req.headers.authorization){
+          return res.status(401).send({message: 'Forbidden Access'});
+        }
+        const token = req.headers.authorization.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+          if(err){
+            return res.status(401).send({message: 'Forbidden Access'})
+          }
+          req.decoded = decoded;
+          next();
+
+        })
+      }
+    // middleware verify token end
+
     // Users related API start
-    app.get('/users', async(req, res) => {
+    app.get('/users', verifyToken, async(req, res) => {
       const result = await userCollection.find().toArray();
       res.send(result);
     })
+
+
+    app.get('/users/admin/:email', verifyToken, async(req, res) => {
+      const email = req.params.email;
+      if(email !== req.decoded.email){
+        return req.status(403).send({message: 'Unauthorized Access'})
+      }
+      const query = {email: email};
+      const user = await userCollection.findOne(query);
+      let admin = false;
+      if(user){
+        admin = user?.role === 'admin';
+      }
+      res.send({admin})
+
+    })
+
      app.post('/users', async(req, res) => {
       const user = req.body;
       // insert email if user does not exist
