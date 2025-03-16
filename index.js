e9fac6427e7643c24d403d614fba7ce6e6af297e
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-03-16 22:41:00 +0600

    Create menu post Api

--- index.js
+++ index.js
@@ -136,6 +136,12 @@ async function run() {
       }
     });
 
+    app.post('/menu', async(res, req) => {
+      const item = req.body;
+      const result = await menuCollection.insertOne(item);
+      res.send(result);
+    })
+
 
     app.get('/reviews', async(req, res) => {
         const result = await reviewCollection.find().toArray();
