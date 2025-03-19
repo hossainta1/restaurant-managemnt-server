1ef6fca319aabece0c2d517ed3c4d8f725be8621
Tanvir Hossain <tanvir.hossain303030@gmail.com>
2025-03-19 22:19:58 +0600

    Update and manage menu API

--- index.js
+++ index.js
@@ -42,33 +42,39 @@ async function run() {
     // Jwt related API end
 
     // middleware verify token start
+      // jwt related api
+      app.post('/jwt', async (req, res) => {
+        const user = req.body;
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+        res.send({ token });
+      })
+  
+      // middlewares 
       const verifyToken = (req, res, next) => {
-        console.log('inside verify token', req.headers.authorization);
-        if(! req.headers.authorization){
-          return res.status(401).send({message: 'Unauthorized Access'});
+        // console.log('inside verify token', req.headers.authorization);
+        if (!req.headers.authorization) {
+          return res.status(401).send({ message: 'unauthorized access' });
         }
         const token = req.headers.authorization.split(' ')[1];
         jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-          if(err){
-            return res.status(401).send({message: 'Unauthorized Access'})
+          if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
           }
           req.decoded = decoded;
           next();
-
         })
       }
-
-      const verifyAdmin = async(req, res, next) => {
-
+  
+      // use verify admin after verifyToken
+      const verifyAdmin = async (req, res, next) => {
         const email = req.decoded.email;
-        const query = {email: email};
+        const query = { email: email };
         const user = await userCollection.findOne(query);
         const isAdmin = user?.role === 'admin';
-        if(!isAdmin){
-          return res.send.status(403).send({message: 'Forbidden Access'})
+        if (!isAdmin) {
+          return res.status(403).send({ message: 'forbidden access' });
         }
         next();
-
       }
     // middleware verify token end
 
@@ -125,23 +131,55 @@ async function run() {
 
     })
 
-    // API Route to Get Menu
-    app.get('/menu', async (req, res) => {
-      try {
-        const result = await menuCollection.find().toArray();
-        res.send(result);
-      } catch (error) {
-        console.error("Error fetching menu:", error);
-        res.status(500).send("Internal Server Error");
+    
+   // menu related apis
+   app.get('/menu', async (req, res) => {
+    const result = await menuCollection.find().toArray();
+    res.send(result);
+  });
+
+  app.get('/menu/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await menuCollection.findOne(query);
+    res.send(result);
+  })
+
+  app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
+    const item = req.body;
+    const result = await menuCollection.insertOne(item);
+    res.send(result);
+  });
+
+  app.patch('/menu/:id', async (req, res) => {
+    const item = req.body;
+    const id = req.params.id;
+    const filter = { _id: new ObjectId(id) }
+    const updatedDoc = {
+      $set: {
+        name: item.name,
+        category: item.category,
+        price: item.price,
+        recipe: item.recipe,
+        image: item.image
       }
-    });
+    }
 
-    app.post('/menu', verifyToken, verifyAdmin, async(req, res) => {
-      const item = req.body;
-      const result = await menuCollection.insertOne(item);
-      res.send(result);
-    })
+    const result = await menuCollection.updateOne(filter, updatedDoc)
+    res.send(result);
+  })
 
+  app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+    const id = req.params.id;
+    const query = { _id: new ObjectId(id) }
+    const result = await menuCollection.deleteOne(query);
+    res.send(result);
+  })
+
+   // menu related apis end
+
+
+    // Review related api
 
     app.get('/reviews', async(req, res) => {
         const result = await reviewCollection.find().toArray();
